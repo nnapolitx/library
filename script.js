b6ec1669f978f006b8a9f8778ce0262b77f631ed
nnapolitx <nnapolitano88@gmail.com>
2022-10-07 20:55:25 -0300

    adding just the basics of the script from the *main branch

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 let library =[]
+const inputTitle = document.querySelector('#input-title')
+const inputAuthor = document.querySelector('#input-author')
+const inputPages = document.querySelector('#input-pages')
+const inputStatus = document.querySelector('#input-status')
+const newBook = document.querySelector('#new-book')
+const bookshelf = document.querySelector('.bookshelf')
 
+const checkmark = document.createElement('img')
+checkmark.classList.add('.checkmark')
+checkmark.src = 'img/check.png'
+const xbox = document.createElement('img')
+xbox.classList.add('.xbox')
+xbox.src = 'img/xbox.png'
 
+const newBook = document.createElement('div')
+newBook.classList.add('.book')
 
-
-
-
-
-
-
-
-/*let myLibrary =[];
-const inputTitle = document.querySelector('#input-title');
-const inputAuthor = document.querySelector('#input-author');
-const inputPages = document.querySelector('#input-pages');
-const inputStatus = document.querySelector('#input-status');
-const newBook = document.querySelector('#new-book');
-const bookShelf = document.querySelector('.book-container');
-
-const title = document.createElement('div');
-title.classList.add('.title');
-const author = document.createElement('div');
-author.classList.add('.author');
-const pages = document.createElement('div');
-pages.classList.add('.pages');
-const stat = document.createElement('div');
-stat.classList.add('.status');
-const remove = document.createElement('div');
-remove.classList.add('.remove');
-
-const checkmark = document.createElement('img');
-checkmark.classList.add('.checkmark');
-checkmark.src = 'img/check.png';
-const xbox = document.createElement('img');
-xbox.classList.add('.xbox');
-xbox.src = 'img/xbox.png';
-
-function Book (title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+function Book (title, author, pages, read){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
 }
+
+function addBooktoLibrary(bk){
+    bk = Object.create(Book)
+    bk.title = inputTitle.value
+    bk.author = inputAuthor.value
+    bk.pages = inputPages.value
+    bk.read = inputStatus.checked
+    myLibrary.push(bk)
+}
+
+
+
+
+
+
+
+
+/*
+
 
 function addBooktoLibrary(bk) {
     bk = Object.create(Book);
