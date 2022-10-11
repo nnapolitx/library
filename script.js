154bf1c0b90aa0abb1862edc511974572f0ed1a5
nnapolitx <nnapolitano88@gmail.com>
2022-10-11 15:58:45 -0300

    script now detects click for new buttons

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const xbox = document.createElement('img')
 xbox.classList.add('.xbox')
 xbox.src = 'img/xbox.png'
 
-
+let allBtns;
 
 function Book (title, author, pages, read){
     this.title = title
@@ -31,10 +31,64 @@ function addBooktoLibrary(bk){
     library.push(bk)
 }
 
+function shelfBook(bk){
+    
+    console.log('pass')
+    const newBook = document.createElement('ul')
+    newBook.classList.add('.book')
+    bookshelf.appendChild(newBook)
+
+    const title = document.createElement('li')
+    const author = document.createElement('li')
+    const pages = document.createElement('li')
+    const read = document.createElement('li')
+    const remove = document.createElement('li')
+    
+    let v = Object.values(bk);
+    console.log(v);
+    title.textContent=v[0];
+    author.textContent=v[1];
+    pages.textContent=v[2];
+
+    newBook.appendChild(title);
+    newBook.appendChild(author);
+    newBook.appendChild(pages);
+
+    if (v[3]===true){
+        read.textContent='Read';
+        newBook.appendChild(read);
+        read.appendChild(checkmark);
+    } else {
+        read.textContent='Not Read';
+        newBook.appendChild(read)
+        read.appendChild(xbox);
+    }
+    
+    newBook.appendChild(remove);
+    const rembtn = document.createElement('button')
+    rembtn.setAttribute('id', 'remove')
+    rembtn.textContent = 'Remove?'
+    remove.appendChild(rembtn)
+    
+    allBtns=document.querySelectorAll('#remove')
+
+    removeButtonEvent()
+    console.log('done')
+}
+
 addBookBtn.addEventListener('click', (e) => {
     addBooktoLibrary(e)
     shelfBook(library.at(-1))
+    console.log(library.at(-1))
 });
+
+function removeButtonEvent(){
+    for (let j=0; j<library.length+1; j++){//Need to remove +1 after removing example//
+        allBtns[j].addEventListener('click', (e)=>{
+            console.log(e)
+        })
+    }
+}
 
 function findBook(b) {
     let indexNum = library.map(object=>object.title).indexOf(b);
@@ -47,59 +101,3 @@ function deleteBook(b) {
     library.splice(i,1);
     return library;
 }
-
-function shelfBook(bk){
-    
-        console.log('pass')
-        const newBook = document.createElement('div')
-        newBook.classList.add('.book')
-        newBook.innerHTML = bk.title
-        bookshelf.appendChild(newBook)
-        console.log('done')
-
-}
-
-/*
-
-
-    
-
-function addBooktoLibrary(bk) {
-    bk = Object.create(Book);
-    bk.title = inputTitle.value;
-    bk.author = inputAuthor.value;
-    bk.pages = inputPages.value;
-    bk.read = inputStatus.checked;
-    myLibrary.push(bk);
-    shelfBook(bk);
-}
-
-newBook.addEventListener('click', (e) => {
-    addBooktoLibrary(e);
-});
-
-function shelfBook(e) {
-    let v = Object.values(e);
-    console.log(v);
-    title.textContent=v[0];
-    author.textContent=v[1];
-    pages.textContent=v[2];
-
-    bookShelf.appendChild(title);
-    bookShelf.appendChild(author);
-    bookShelf.appendChild(pages);
-
-    if (v[3]===true){
-        stat.textContent='Read';
-        bookShelf.appendChild(stat);
-        stat.appendChild(checkmark);
-    } else {
-        stat.textContent='Not Read';
-        bookShelf.appendChild(stat)
-        stat.appendChild(xbox);
-    }
-    
-    bookShelf.appendChild(remove);
-}
-
-*/
